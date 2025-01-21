1d7acd638db844f2a347c5e866aa96012de67429
nshriki <brensohon731@gmail.com>
2025-01-21 22:08:11 +0800

    V1 Update 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.common)
     implementation(libs.firebase.database)
+    implementation(libs.androidx.gridlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
