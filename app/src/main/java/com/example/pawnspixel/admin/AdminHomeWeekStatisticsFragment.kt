50378f6f53bb4c7960330616ec0989a47a7d033a
nshriki <brensohon731@gmail.com>
2025-02-04 21:08:52 +0800

    Update: Admin Home & Navbar

---
+++ app/src/main/java/com/example/pawnspixel/admin/AdminHomeWeekStatisticsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.pawnspixel.admin
+
+import androidx.fragment.app.Fragment
+import com.example.pawnspixel.R
+
+class AdminHomeWeekStatisticsFragment : Fragment(R.layout.fragment_admin_home_week)
---
+++ app/src/main/java/com/example/pawnspixel/admin/AdminHomeYearStatisticsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.pawnspixel.admin
+
+import androidx.fragment.app.Fragment
+import com.example.pawnspixel.R
+
+class AdminHomeYearStatisticsFragment : Fragment(R.layout.fragment_admin_home_year)
